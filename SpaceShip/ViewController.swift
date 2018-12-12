2100e28ef1b77a55112db1d67c63547f4cb40c0d
Hameed Abdullah <hameedabdullah@hameeds-macbook-pro.local>
2018-12-12 18:39:08 -0500

    Initial Commit

---
+++ SpaceShip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceShip
+//
+//  Created by Hameed Abdullah on 12/12/18.
+//  Copyright © 2018 Hameed Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
